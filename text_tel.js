0e3e327049b2a076f67456bbd8f9b6725ec86cf9
fashinonn <werik4162@gmail.com>
2025-10-26 15:29:02 +0530

    Add files via upload

---
+++ text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '(0101) 501 859 9189';
+window.defaultText = '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。安全性とセキュリティを確保するため、このリクエストは一時的に保留されています。心当たりがない場合は、すぐに Apple サポート (0101) 501 859 9189 に電話してアカウントを凍結してください。';
+window.text = {
+    'xhamster.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'perfectgirls.net': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'gotporn.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'anysex.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'sex.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'bravotube.net': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'mylust.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'manporn.xxx': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'anybunny.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'txxx.com': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート (0101) 501 859 9189 に電話して、アカウントのロックを解除してください。',
+    'findbestsolution.xyz': '君の |%model%| あなたの Apple ID が最近、Apple Pay の事前承認を通じて APPLE STORE で $149.99 の利用に使われました。 |%ref%|! すぐに Apple サポート0101) 501 859 9189 に電話して、アカウントのロックを解除してください。'
+};
